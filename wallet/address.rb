27e48ee75b449efe657d9671d64fd413c7e685f5
kanna <kanna@vivaldi.net>
2018-04-09 00:36:24 +0900

    add get_utxo method

--- wallet/address.rb
+++ wallet/address.rb
@@ -3,5 +3,15 @@ class Address
 
   def initialize(*address_pack)
     @address, @priv_key, @pub_key, @pub_key_hash160 = address_pack[0], address_pack[1], address_pack[2], address_pack[3]
+    @utxo = []
+  end
+
+  def get_utxo
+    address = JSON.parse(Typhoeus.get("https://api.blockcypher.com/v1/btc/test3/addrs/#{@address}?unspentOnly=true").body)
+    transactions = address["txrefs"]
+    transactions.each do |tx|
+      @utxo.push([tx['tx_hash'], tx["tx_output_n"], tx['value']/100000000.0])
+    end
+    @utxo
   end
 end
